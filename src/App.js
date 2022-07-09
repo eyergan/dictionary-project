cb3f5a90bc598c8c129467fd29992e2cfb7a05db
eyergan <eyergan1@gmail.com>
2022-07-09 19:05:20 -0400

    added footer

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">Dictionary</header>
+    <div className="App mt-5">
+      <header className="App-header">
+        <h1>Dictionary</h1>
+      </header>
+      <footer className="text-center">
+        Coded by Evy Yergan and Open Sourced
+      </footer>
     </div>
   );
 }
