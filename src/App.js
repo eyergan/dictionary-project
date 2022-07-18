bcc2d3d591c58c47402916c3460b28de44478bce
eyergan <eyergan1@gmail.com>
2022-07-17 20:16:45 -0400

    styled application

--- src/App.js
+++ src/App.js
@@ -6,13 +6,26 @@ function App() {
   return (
     <div className="App mt-5">
       <header className="App-header">
-        <h1>Dictionary</h1>
+        <h1>📚 dictionary</h1>
       </header>
       <main>
-        <Dictionary />
+        <Dictionary defaultKeyword="cloud" />
       </main>
       <footer className="App-footer">
-        <small>Coded by Evy Yergan and Open Sourced</small>
+        <small>
+          Coded by{" "}
+          <a href="https://evyyergan.com" target="_blank" rel="noreferrer">
+            Evy Yergan
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://github.com/eyergan/dictionary-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open Sourced
+          </a>
+        </small>
       </footer>
     </div>
   );
--- src/Meaning.js
+++ src/Meaning.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Synonyms from "./Synonyms";
+import "./Meaning.css";
 export default function Meaning(props) {
   console.log(props.meaning);
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
 
-      <p>{props.meaning.definitions[0].definition}</p>
+      <div className="definition">
+        {props.meaning.definitions[0].definition}
+      </div>
       <br />
       <Synonyms synonyms={props.meaning.synonyms} />
     </div>
